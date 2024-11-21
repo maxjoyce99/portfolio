13ccb396d40086f763d1959683ecc76cca38425a
Max <maxjoyce99@gmail.com>
2024-11-21 17:46:29 -0500

    Added a basic skeleton for the page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Max Joyce Portfolio website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+      <p>Maxwell Joyce
+        maxjoyce99@gmail.com
+        774-278-1935
+      </p>
+    </header>
+
+    <body className="App">
+      <div>
+        <h2>Hi, I'm Max Joyce</h2>
+        <p>Fullstack software engineer with experience using Javascript, React, Node.js, HTML, CSS, and MongoDB to create web applications. Experience using Docker and Kubernetes to containerize and deploy web applications. Also proficient with SQL, QML, Python,  and C++.</p>
+      </div>
+      <section>
+        <h2>Skills</h2>
+        <div class="all_skills">
+          <div class="skill">
+            <img src="html_finalprojimages/html5.png"/>
+            <h6>HTML</h6>
+            <p>2 years experience</p>
+          </div>  
+
+          <div class="skill">
+            <img src="html_finalprojimages/js.jpeg"/>
+            <h6>JavaScript</h6>
+            <p>3 years experience</p>
+          </div>  
+        </div>
+        
+
+      </section>
+
+      <div>
+        <h2>Projects</h2>
+        
+        <a href="https://wherehaveibeenfrontend-production.up.railway.app/">WhereHaveIBeen</a>
+
+      </div>
+    </body>
+    </>
   );
 }
 
