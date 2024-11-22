feadc7708ffaaa4f4cb9b968230b514eedad085e
Max <maxjoyce99@gmail.com>
2024-11-22 10:22:14 -0500

    Added a navbar to scroll to the sections with a dark theme, contact info on the right

--- src/App.js
+++ src/App.js
@@ -12,18 +12,36 @@ import me_hot_spring from './images/me_hot_spring.jpg'
 function App() {
   return (
     <>
-    <header>
-      <p>Maxwell Joyce
-        maxjoyce99@gmail.com
-        774-278-1935
-        Make a menu to scroll down faster
-      </p>
+    <header class="d-flex flex-box w-100 bg-dark" style={{ height: '75px' }}>
+
+      <nav class="navbar navbar-expand navbar-dark w-75 h-100">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#about">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#projects">Projects</a>
+          </li>
+        </ul>
+
+        
+      </nav>
+
+      <div class ="w-25 h-100">
+            <p class ="text-end p-2" >Maxwell Joyce<br></br>
+            maxjoyce99@gmail.com |
+            774-278-1935
+            </p>
+        </div>
     </header>
 
     <body className="App">
-      <section class="w-75 mx-auto" style={{ height: '300px' }}>
-        <div class="d-flex flex-box h-100">
-          <img class ="w-25 h-100 m-2" src={me_hot_spring}/>
+      <section id="about" class="w-75 mx-auto" style={{ height: '300px' }}>
+        <div class="d-flex justify-content-center flex-box h-100 p-2">
+          <img class ="w-25 h-100" src={me_hot_spring}/>
           <div class="w-50 h-75 m-2">
             <h2 class=" w-50 h-25">Hi, I'm Max Joyce!</h2>
             <p class="w-100 h-50"> I am a fullstack software engineer with experience using Javascript, React, Node.js, HTML, CSS, and MongoDB to create web applications. I also have experience using Docker and Kubernetes to containerize and deploy web applications. Also proficient with SQL, QML, Python,  and C++.</p>
@@ -36,7 +54,7 @@ function App() {
         </div>
       </section>
 
-      <section class="w-50 mx-auto" style={{ height: '300px' }}>
+      <section id="skills" class="w-50 mx-auto" style={{ height: '300px' }}>
         <h2>Skills</h2>
         <div class="row justify-content-xl-between h-50" >
           <div class="skills_card col border border-white h-100 p-2 m-2 rounded ">
@@ -69,7 +87,7 @@ function App() {
 
       </section>
 
-      <section class="w-75 mx-auto" style={{ height: '500px' }}>
+      <section id="projects" class="w-75 mx-auto" style={{ height: '500px' }}>
         <h2>Projects</h2>
         <div class="d-flex flex-row mb-3 justify-content-center justify-content-between">
           <div class="rounded border w-50 p-2 m-2">
